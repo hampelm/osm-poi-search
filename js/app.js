0a22890248db5d1db2226b00dd0a24bfbbbd21f1
Matt Hampel <matth@localdata.com>
2013-11-08 10:24:54 -0500

    Churches

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(function(){
 	// http://www.overpass-api.de/api/xapi_meta?*%5Bname=Sylt%5D
 	// http://www.overpass-api.de/api/xapi?node[natural=tree][bbox=-83.12737,42.31794,-83.0175,42.37681][out:json]
 	// http://www.overpass-api.de/api/xapi?node["natural"="tree"][bbox=-83.11981201171875,42.3883461218366,-82.92016983032227,42.45170800982129]
-  var map = L.map('map').setView([42.42, -83.02 ], 10);
+  var map = L.map('map').setView([42.42, -83.02 ], 13);
 
   baseLayer = L.tileLayer('http://a.tiles.mapbox.com/v3/matth.map-zmpggdzn/{z}/{x}/{y}.png');
   map.addLayer(baseLayer);
@@ -12,11 +12,46 @@ $(function(){
   var sw = bounds.getSouthWest();
 	console.log(ne, sw);
 
+	var pairs = [
+		{
+			key: 'natural',
+			value: 'tree'
+		},
+		{
+			key: 'amenity',
+			value: 'place_of_worship',
+			name: 'name'
+		}
+	];
+
+	var queryMaker = function(pair) {
+		return '[' + pair.key + '=' + pair.value + ']';
+	}
+
 	// omg this is the worst
-	var text = 'node[natural=tree][bbox=' + sw.lng + ',' + sw.lat + ',' + ne.lng + ',' + ne.lat + ']';
-	// nw, se
+	var pair = queryMaker(pairs[1]);
+	var text = 'node' + pair + '[bbox=' + sw.lng + ',' + sw.lat + ',' + ne.lng + ',' + ne.lat + ']';
 	url = baseUrl + text;
-	console.log(url);
+
+	var processOSM = function(data) {
+		console.log(data);
+		var nodes = $(data).find('node');
+		console.log(nodes);
+		$.each(nodes, function(index, node) {
+			console.log(node);
+			var lat = node.getAttribute('lat');
+			var lng = node.getAttribute('lon');
+			console.log(lat,lng);
+
+			// node.find(tag attribute k=name get attribute v);
+
+			L.marker([lat,lng]).addTo(map).bindPopup()
+    		.openPopup();
+		});
+	};
+	var query = $.get(url)
+	query.done(processOSM);
+
 });
 
 
